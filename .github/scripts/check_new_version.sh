e959e7c33d5e85a91af0d795435d56a7612a2f87
Pablo Clemente <paclema@gmail.com>
2023-04-26 15:30:35 +0200

    Prettify

--- .github/scripts/check_new_version.sh
+++ .github/scripts/check_new_version.sh
@@ -8,7 +8,7 @@ echo "Checking PlatformIO Registry library: $username/$package_name"
 
 current_version=$(jq -r '.version' library.json)
 latest_version=$(pio package search "$username/$package_name" | grep -Eo "Library • [^ ]+ " | sed -e 's/Library • //')
-echo "New version: ($current_version)   -   Latest version: ($latest_version)"
+echo "New version: v$current_version   -   Latest version: v$latest_version"
 
 if [[ "$current_version" != "$(echo -e "$current_version\n$latest_version" | sort -V | tail -n1)" ]]; then
     echo "Error: The new version ($current_version) is not greater than latest version ($latest_version)"
