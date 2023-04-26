e6cf9020b379542c9d3e32ebc8cab18712523599
Pablo Clemente <paclema@gmail.com>
2023-04-26 14:29:30 +0200

    Updated check new version on workflow

---
+++ .github/scripts/check_new_version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# set -x
+
+package_name=$(jq -r '.name' library.json)
+username=$(echo "$GITHUB_REPOSITORY" | cut -d/ -f1)
+echo "Checking PlatformIO Registry library: $username/$package_name"
+
+current_version=$(jq -r '.version' library.json)
+latest_version=$(pio package search "$username/$package_name" | grep -Eo "Library • [^ ]+ " | sed -e 's/Library • //')
+echo "New version: ($current_version)   -   Latest version: ($latest_version)"
+
+if [[ "$current_version" != "$(echo -e "$current_version\n$latest_version" | sort -V | tail -n1)" ]]; then
+    echo "Error: The new version ($current_version) is not greater than latest version ($latest_version)"
+    exit 1
+fi
+
+# set +x
